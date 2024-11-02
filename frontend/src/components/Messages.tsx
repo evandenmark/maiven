27f24168b3f096185c133956462610e24f3834e0
evandenmark <evanlewisdenmark@gmail.com>
2024-11-01 14:42:57 -1000

    remove unnecssary

--- frontend/src/components/Messages.tsx
+++ frontend/src/components/Messages.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Autocomplete, Button, Paper, Stack, styled, TextField, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
 import { useAuth0 } from "@auth0/auth0-react";
-import { User } from "../types";
 
 type Message = {
     id: number;
